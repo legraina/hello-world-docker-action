701c86c1c3558b018911941dffd2ff043fa1d2e2
Bright Ran <46307996+BrightRan@users.noreply.github.com>
2019-11-26 11:29:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,5 @@
 #!/bin/sh -l
-echo "**********************************"
-cd ..
-pwd
-echo "========================"
-ls
-echo "**********************************"
-cd ..
-pwd
-echo "========================"
-ls
-echo "**********************************"
-cd home
-cd runner
-cd work
-cd TestClock
-pwd
-echo "========================"
-ls
-echo "**********************************"
+
 cd $1
 echo "Current working directory is $PWD"
 ls
