bff67e39436fd28fbb7c87ef260c96eb59ce59a7
Bright Ran <46307996+BrightRan@users.noreply.github.com>
2020-03-11 13:52:05 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,12 @@ echo "Meeting at $time"
 
 echo "run mkdir -p $HOME/.m2/repository"
 mkdir -p $HOME/.m2/repository
+echo "This is a test file." > $HOME/.m2/repository/test.txt
+
+cat $HOME/.m2/repository/test.txt
+
 echo "********************************ls in #HOME********************************"
 echo $HOME
 ls -lah $HOME
 echo "******************************ls in $HOME/.m2******************************"
-ls -lah $HOME/.m2
+ls -lah $HOME/.m2/repository
