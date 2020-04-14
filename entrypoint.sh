e309e01b51edff2935688aa14c9a7578bfcb0cfb
Bright Ran <46307996+BrightRan@users.noreply.github.com>
2020-04-14 14:10:02 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,5 @@ echo "Hello $1"
 time=$(date)
 echo "Meeting at $time"
 
-echo "run mkdir -p $HOME/.m2/repository"
-mkdir -p $HOME/.m2/repository
-echo "This is a test file." > $HOME/.m2/repository/test.txt
-
-cat $HOME/.m2/repository/test.txt
-
-echo "********************************ls in #HOME********************************"
-echo $HOME
-ls -lah $HOME
-echo "******************************ls in $HOME/.m2******************************"
-ls -lah $HOME/.m2/repository
+echo "$PWD"
+echo "$GITHUB_WORKSPACE"
